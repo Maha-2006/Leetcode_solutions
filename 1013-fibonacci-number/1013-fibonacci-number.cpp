ab249ecd8910754028508197b2304434d0ad7853
Maha-2006 <163001517+Maha-2006@users.noreply.github.com>
2025-06-21 19:17:39 +0530

    Time: 0 ms (100%), Space: 7.9 MB (36.76%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<2) return n;
+        if(n<=1) return n;
         int a=0,b=1,c;
-        for(int i=2;i<=n;i++)
+        for(int i=1;i<n;i++)
         {
             c = a+b;
             a = b;
