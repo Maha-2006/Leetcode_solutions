1e881142b49f1ac69d8df7dbd1cd7d6c8aee9e17
Maha-2006 <163001517+Maha-2006@users.noreply.github.com>
2025-06-25 17:51:04 +0530

    Time: 729 ms (49.61%), Space: 55.3 MB (66.67%) - LeetHub

---
+++ 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.java
@@ -0,0 +1,52 @@
+class Solution {
+    public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
+        long low = -100_000L * 100_000L;
+        long high = 100_000L * 100_000L;
+        
+        while (low < high) {
+            long mid = low + (high - low) / 2;
+            if (countPairsLessEqual(nums1, nums2, mid) < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        
+        return low;
+    }
+    
+    private long countPairsLessEqual(int[] nums1, int[] nums2, long target) {
+        long count = 0;
+        for (int a : nums1) {
+            if (a > 0) {
+               
+                int left = 0, right = nums2.length;
+                while (left < right) {
+                    int mid = left + (right - left) / 2;
+                    if ((long) a * nums2[mid] <= target) {
+                        left = mid + 1;
+                    } else {
+                        right = mid;
+                    }
+                }
+                count += left;
+            } else if (a < 0) {
+                
+                int left = 0, right = nums2.length;
+                while (left < right) {
+                    int mid = left + (right - left) / 2;
+                    if ((long) a * nums2[mid] <= target) {
+                        right = mid;
+                    } else {
+                        left = mid + 1;
+                    }
+                }
+                count += nums2.length - left;
+            } else {
+                if (target >= 0) count += nums2.length;
+            }
+        }
+        return count;
+        
+    }
+}
