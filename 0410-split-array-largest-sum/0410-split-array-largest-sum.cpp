b9b93590b28cba543206422f22c339796224f7f0
Maha-2006 <163001517+Maha-2006@users.noreply.github.com>
2025-07-02 23:08:59 +0530

    Time: 0 ms (100%), Space: 10.4 MB (56.17%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    int splitArray(vector<int>& nums, int k) {
-        int low = *max_element(nums.begin(),nums.end());
-        int high = accumulate(nums.begin(),nums.end(),0);
-        int ans = high;
-
-        while(low<=high){
-            int mid = (low+high)/2;
-            int partitions = 1;
-            int Sum = 0;
-            for(int i=0;i<nums.size();i++){
-                if(Sum + nums[i]>mid){
-                    partitions++;
-                    Sum = nums[i];
-                }else{
-                    Sum += nums[i];
-                }
-            }
-            if(partitions > k){
-               low = mid + 1;
-            }else{ 
-                ans = mid;
-                high = mid - 1;
+     int helper(vector<int>&nums , int mid , int n){
+        int count = 1;
+        int split = 0;
+        for(int i = 0;i<n;i++){
+            if(split + nums[i] <=mid){
+                split+=nums[i];
+            }else{
+                count++;
+                split = nums[i];
             }
         }
-        return ans;
+        return count;
+    }
+
+    int splitArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        int low = *max_element(nums.begin(),nums.end());
+        int high = accumulate(nums.begin(),nums.end(),0);
+
+        while(low<=high){
+            int mid = low + ((high-low)/2);
+            if(helper(nums,mid,n)<=k){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return low;
+        
     }
 };
