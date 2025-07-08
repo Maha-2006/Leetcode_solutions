f8f30570a658839752ebffa0bb7b71237c449396
Maha-2006 <163001517+Maha-2006@users.noreply.github.com>
2025-07-08 23:21:48 +0530

    Time: 0 ms (100%), Space: 95.2 MB (43.76%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,12 +3,12 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size();
         int n2 = nums2.size();
-        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
+        if(n1>n2) return findMedianSortedArrays(nums2,nums1);//swap if n1>n2
 
         int n = n1+n2;
-        int left = (n1+n2+1)/2;
+        int left = (n1+n2+1)/2;//for both odd and even they are same formulae
 
-        int low = 0, high = n1;
+        int low = 0, high = n1;// taking max elements n1 is possible
         while(low<=high){
             int mid1 = (low+high) >>1;
             int mid2 = left - mid1;
@@ -21,8 +21,8 @@ public:
             if(mid2-1>=0)l2 = nums2[mid2-1];
 
             if(l1<=r2 && l2<=r1){
-                if(n%2==1) return max(l1,l2);
-                else return ((double)(max(l1,l2)+min(r1,r2)))/2.0;
+                if(n%2==1) return max(l1,l2);//for odd
+                else return ((double)(max(l1,l2)+min(r1,r2)))/2.0;//for even
             }
 
             else if(l1>r2) high = mid1 - 1;
