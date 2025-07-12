85148171981a980b8c780e1276d2265a9a595eeb
Maha-2006 <163001517+Maha-2006@users.noreply.github.com>
2025-07-12 21:12:31 +0530

    Time: 0 ms (100%), Space: 9.2 MB (43.17%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        string ans = "";
+        string ans = "";//empty space will be created
         int carry = 0;
-        int i = num1.size()-1;
+        int i=num1.size()-1;
         int j = num2.size()-1;
 
-        while(i>=0 || j>=0 || carry){
+        while(i>=0 || j>=0 ||carry){
             if(i>=0){
                 carry += num1[i]-'0';
                 i--;
             }
             if(j>=0){
-                carry+=num2[j]-'0';
+                carry += num2[j]-'0';
                 j--;
             }
             ans += carry%10+'0';
-            carry/=10;
-            
+            carry /= 10;
         }
-        reverse(ans.begin(), ans.end());
-        return ans;
-
+         reverse(ans.begin(),ans.end());
+    return ans;
     }
 };
